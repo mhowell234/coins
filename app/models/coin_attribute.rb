a906b8552bf4255c59f063625ac0727c9a33856d
Matt Howell <mhowell234@gmail.com>
2013-03-25 21:13:07 -0700

    Adding coin attribute type model

--- app/models/coin_attribute.rb
+++ app/models/coin_attribute.rb
@@ -2,5 +2,6 @@ class CoinAttribute < ActiveRecord::Base
   attr_accessible :coin_attribute_type_id, :value, :coin_id
   
   belongs_to :coin
+  belongs_to :coin_attribute_type
 
 end
--- app/models/coin_attribute_type.rb
+++ app/models/coin_attribute_type.rb
@@ -1,3 +1,6 @@
 class CoinAttributeType < ActiveRecord::Base
   attr_accessible :value
+  
+  has_many :coin_attribute_types
+  
 end
